a8f633f09d4f64bb8694244e0240aaacf00d7a53
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-11-16 14:40:20 -0300

    fix bug

--- src/components/header-app.vue
+++ src/components/header-app.vue
@@ -3,7 +3,7 @@
     <nav class="navbar navbar-dark bg-dark">
       <div class="container-fluid">
         <a href="/" class="navbar-brand">Desafio Unidac - Grupo WL</a>
-        <a href="@/components/form.vue" class="btn btn-light">Cadastrar</a>
+        <a href="/form" class="btn btn-light">Cadastrar</a>
       </div>
     </nav>
   </header>
